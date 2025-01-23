e39c84aa0eab5aa3ab7a3a9eaafe899ea4e93e90
Sagar Bhakat <sagarbhakat12345@gmail.com>
2025-01-23 11:35:24 +0530

    QR code generator

---
+++ QR-Code-generator/main.js
@@ -0,0 +1,34 @@
+const QRcontainer = document.querySelector(".qr-container");
+const qrTextInput = document.querySelector(".qr-text");
+const generateQRBtn = document.querySelector(".generateQrcodeBtn");
+const errorMessage = document.querySelector(".error-message");
+
+
+generateQRBtn.addEventListener("click", () => {
+    validateInputField();
+
+})
+
+function validateInputField() {
+    console.log(qrTextInput.value);
+    if (qrTextInput.value.trim().length > 0) {
+        generateQRcode();
+    } else {
+        errorMessage.textContent = "Please enter a valid input";
+        QRcontainer.innerHTML = "";
+    }
+}
+
+function generateQRcode() {
+    QRcontainer.innerHTML = "";
+    new QRCode(QRcontainer, {
+        text: qrTextInput.value,
+        width: 400,
+        height: 400,
+        colorLight: "#fff",
+        colorDark: "#000",
+    });
+
+    qrTextInput.value = "";
+    errorMessage.textContent = "";
+}
